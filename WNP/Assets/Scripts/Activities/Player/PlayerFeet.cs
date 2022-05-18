b7c5700d21c279a5376d260062b07bfb6073c44a
jimJunhyun <tsumego2015@gmail.com>
2022-05-18 18:38:43 +0900

    문제 해결
    병합됨.

--- WNP/Assets/Scripts/Activities/Player/PlayerFeet.cs
+++ WNP/Assets/Scripts/Activities/Player/PlayerFeet.cs
@@ -1,47 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class PlayerFeet : MonoBehaviour
 {
-	public float rad = 0.3f;
-	public LayerMask ignoreLayer;
-	Collider2D feetCol;
-	private void Start()
+	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		ignoreLayer = ~ignoreLayer;
-	}
-	private void Update()
-	{
-		
-		feetCol = Physics2D.OverlapCapsule(transform.position, new Vector2(1,1f), CapsuleDirection2D.Horizontal,0, ignoreLayer);
-		if (!feetCol)
+		if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Fallable"))
 		{
-			PlayerController.Instance.isGrounded = false;
-		}
-		else if ((feetCol.CompareTag("Ground") || feetCol.CompareTag("Fallable")) && Approximate(PlayerController.Instance.rig.velocity.y, 0, 0.2f))
-		{
-			PlayerController.Instance.isGrounded = true;
-		}
-		else
-		{
-			PlayerController.Instance.isGrounded = false;
+			PlayerController.isGrounded = true;
 		}
 	}
-	private void OnDrawGizmos()
+	private void OnTriggerExit2D(Collider2D collision)
 	{
-		Gizmos.DrawWireSphere(transform.position, rad);
-	}
-	public static bool Approximate(float a, float b, float range)
-	{
-		if(a > b - range && a < b + range)
+		if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Fallable"))
 		{
-			return true;
-		}
-		else
-		{
-			return false;
+			PlayerController.isGrounded = false;
 		}
 	}
 }
